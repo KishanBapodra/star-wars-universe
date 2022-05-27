3c7909f50a7d32eeda741e5062a8beb854cba3ac
kishan <kishanbapodra2002@gmail.com>
2022-05-28 03:10:43 +0400

    Added cards for movies and shows

--- src/components/movies/movies.js
+++ src/components/movies/movies.js
@@ -1,26 +1,41 @@
 import React from "react";
 import axios from "axios";
 import { useState, useEffect } from "react";
+import Card from "../cards/movie-card";
 
 const Movies = () => {
 
-    const [appState, SetAppState] = useState({loading: false, movies: null});
-    
+    const [isLoading, setLoading] = useState(true)
+    const [appState, SetAppState] = useState(null)
+
     useEffect(() => {
-        SetAppState({loading: true})
         const apiurl = "https://api.themoviedb.org/3/search/movie?api_key="+process.env.REACT_APP_API_KEY+"&language=en-US&query=Star%20Wars&page=1&include_adult=false"
         axios.get(apiurl).then((moviesData) => {
             const starWarsMovies = moviesData.data;
-            SetAppState({loading: false ,movies: starWarsMovies});
+            SetAppState(starWarsMovies);
+            setLoading(false);
         });
-
     }, []);
+    // console.log(appState.results[3].original_title) // for title
 
+    if(!isLoading) {
+        return(
+            appState.results.map((movie) => {
+                console.log(movie);
+                return(
+                <div className="m-8 w-screen">
+                    <Card title={movie.original_title} description={movie.overview} image={movie.poster_path} />
+                </div>
+                )
+            })
+        )
+    } else
     return(
         <>
-            <h1>Movies</h1>
+            <h1>Loading...</h1>
         </>
     );
+    
 }
 
 export default Movies;
--- src/components/tvShows/tvshows.js
+++ src/components/tvShows/tvshows.js
@@ -1,23 +1,37 @@
 import React from "react";
 import axios from "axios";
 import { useState, useEffect } from "react";
+import Card from "../cards/movie-card";
 
 const Shows = () => {
     
-    const [appState, SetAppState] = useState({shows: null});
+    const [isLoading, setLoading] = useState(true);
+    const [appState, SetAppState] = useState(null);
     
     useEffect(() => {
         const apiurl = "https://api.themoviedb.org/3/search/tv?api_key="+process.env.REACT_APP_API_KEY+"&language=en-US&page=1&query=Star%20Wars&include_adult=false"
         axios.get(apiurl).then((showsData) => {
             const starWarsShows = showsData.data;
-            SetAppState({shows: starWarsShows});
+            SetAppState(starWarsShows);
+            setLoading(false);
         });
 
     }, []);
 
+    console.log(appState)
+    if(!isLoading) {
+        return(
+            appState.results.map((show) => {
+                console.log(show);
+                return(
+                   <Card title={show.original_name} description={show.overview} image={show.poster_path}/>
+                )
+            })
+        )
+    } else
     return(
         <>
-            <h1>Shows</h1>
+            <h1>Loading...</h1>
         </>
     );
 }
