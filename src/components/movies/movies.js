1129e36599bd6de66ae97ed04e4a065d7e34614e
kishan <kishanbapodra2002@gmail.com>
2022-05-27 20:12:54 +0400

    Added api calls for fetching movies and shows

--- src/components/movies/movies.js
+++ src/components/movies/movies.js
@@ -1,6 +1,21 @@
 import React from "react";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 const Movies = () => {
+
+    const [appState, SetAppState] = useState({loading: false, movies: null});
+    
+    useEffect(() => {
+        SetAppState({loading: true})
+        const apiurl = "https://api.themoviedb.org/3/search/movie?api_key="+process.env.REACT_APP_API_KEY+"&language=en-US&query=Star%20Wars&page=1&include_adult=false"
+        axios.get(apiurl).then((moviesData) => {
+            const starWarsMovies = moviesData.data;
+            SetAppState({loading: false ,movies: starWarsMovies});
+        });
+
+    }, []);
+
     return(
         <>
             <h1>Movies</h1>
--- src/components/tvShows/tvshows.js
+++ src/components/tvShows/tvshows.js
@@ -1,7 +1,25 @@
 import React from "react";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 const Shows = () => {
-    return(<></>);
+    
+    const [appState, SetAppState] = useState({movies: null});
+    
+    useEffect(() => {
+        const apiurl = "https://api.themoviedb.org/3/search/tv?api_key="+process.env.REACT_APP_API_KEY+"&language=en-US&page=1&query=Star%20Wars&include_adult=false"
+        axios.get(apiurl).then((moviesData) => {
+            const starWarsMovies = moviesData.data;
+            SetAppState({movies: starWarsMovies});
+        });
+
+    }, []);
+
+    return(
+        <>
+            <h1>Shows</h1>
+        </>
+    );
 }
 
 export default Shows;
