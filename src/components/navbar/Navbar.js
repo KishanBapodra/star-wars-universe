579778f568d20e7f73c0d731ba504af64009554a
kishan <kishanbapodra2002@gmail.com>
2022-05-30 23:12:33 +0400

    Added search front-end

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,7 +1,17 @@
 import React from "react";
-import { Link } from "react-router-dom" 
+import { useState } from "react";
+import { Link } from "react-router-dom"
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Navbar = () => {
+
+    const [searchClicked, setSearchClicked] = useState(false);
+
+    function handleSearch() {
+        setSearchClicked(!searchClicked);
+    }
+
     return(
         <nav className="absolute bg-gray-900 bg-opacity-30 w-screen top-0 z-50 flex flex-wrap items-center justify-between px-2 py-5 text-white/75">
             <div className="container font-galaxy text-xl  mx-auto flex flex-wrap items-center justify-between">
@@ -20,6 +30,22 @@ const Navbar = () => {
                     </li>
                     <li className="px-6 hover:text-white/100 hover:font-bold">
                         <Link to="movies">Movies</Link>      
+                    </li>
+                    <li className="px-6 hover:text-white/100 hover:font-bold">
+                    {!searchClicked? <button onClick={handleSearch}>
+                                        <FontAwesomeIcon icon={faSearch} />
+                                    </button> :
+                                    <input 
+                                        className=" font-sans font-normal bg-opacity-40 bg-transparent rounded-lg border-white border pl-2 mb-2 text-base"
+                                        type="text" 
+                                        placeholder="Search" 
+                                        onKeyPress={event => {
+                                            if (event.key === "Enter") {
+                                                handleSearch();
+                                            }
+                                        }}
+                                    />
+                    }
                     </li>
                 </ul>
             </div>
