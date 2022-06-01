1bac1fc6198da18b32d81fb177835b291b5230d3
kishan <kishanbapodra2002@gmail.com>
2022-06-01 19:14:23 +0400

    Add images

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,12 @@ module.exports = {
         "star-wars-2": "url('/src/components/assets/star-wars-2.jpg')",
         "star-wars-3": "url('/src/components/assets/star-wars-3.jpg')",
         "star-wars-4": "url('/src/components/assets/star-wars-4.jpg')",
+        "star-wars-5": "url('/src/components/assets/star-wars-5.jpg')",
+        "star-wars-6": "url('/src/components/assets/star-wars-6.jpg')",
       },
+      boxShadow: {
+        '3xl': '0 35px 60px -15px rgba(0, 0, 0, 0.3)',
+      }
     },
   },
   plugins: [],
