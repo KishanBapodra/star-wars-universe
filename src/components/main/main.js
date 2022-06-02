6c473b3c898fcad6cf3ac94786d4e85d1ce6e8b2
kishan <kishanbapodra2002@gmail.com>
2022-06-02 21:23:32 +0400

    Style: landing page
    minor style change on landing page

--- src/components/main/main.js
+++ src/components/main/main.js
@@ -4,9 +4,9 @@ import Nav from "../navbar/nav";
 const MainPage = () => {
   return (
     <>
-        <div className="bg-star-wars-2">
+        <div className="bg-star-wars-2 flex flex-col h-screen">
           <Nav />
-          <div className="text-gray-200 font-medium text-4xl font-galaxy flex justify-center items-center h-screen">
+          <div className="text-gray-200 font-medium text-4xl font-galaxy h-screen flex justify-center items-center ">
             A long time ago in a galaxy far, far away . . .
           </div>
         </div>
