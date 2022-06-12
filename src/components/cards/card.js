f03ce9493ca12804702c097eede6998fd1f7c2a0
kishan <kishanbapodra2002@gmail.com>
2022-06-12 21:49:21 +0400

    Style: Improved app responsiveness

--- src/components/cards/card.js
+++ src/components/cards/card.js
@@ -6,7 +6,6 @@ const Card = ({data ,title, description, image}) => {
   return(
         <div className="flex flex-column w-[10rem] h-[24rem] shadow-md hover:shadow-[0_45px_85px_-18px_rgba(0,0,0,1)] text-ellipsis overflow-hidden">
             <div className="rounded-lg shadow-lg max-w-xs text-ellipsis bg-white bg-opacity-40 ">
-              {/* <Link to={title.split(" ").join("-").split(":").join("").toLowerCase()}> */}
               <Link to={title.replace(/\//g,'-').replace(/\s+/g,'-').replace(/:+/g,'').toLowerCase()} state={{data: data}}>
                 <img className="rounded-t-lg align-middle" src={"https://image.tmdb.org/t/p/w200/"+image} alt=""/>
                 <div className="p-6">
--- src/components/movies/movies.js
+++ src/components/movies/movies.js
@@ -27,10 +27,10 @@ const Movies = () => {
     if(!isLoading) {
         return(
             <div className="bg-star-wars-6 bg-cover">
-                <div className="grid pt-[7.2rem] pb-10 w-full 3xl:grid-cols-8 2xl:grid-cols-7 xl:grid-cols-6 lg:grid-cols-5 md:grid-cols-4 sm:grid-cols-3">   
+                <div className="flex flex-wrap justify-around sm:m-0 sm:grid pt-[7.2rem] pb-10 w-full 3xl:grid-cols-8 2xl:grid-cols-7 xl:grid-cols-6 lg:grid-cols-5 md:grid-cols-4 sm:grid-cols-3">   
                     {movies.map((movie, index) => {
                         return(
-                            <li key={index} className="pl-10 mt-5">
+                            <li key={index} className="sm:pl-10 mt-5">
                                 <Card className="w-96" data={movie} title={movie.original_title} description={movie.overview} image={movie.poster_path} />
                             </li>
                             )
