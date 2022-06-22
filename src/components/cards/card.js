1b4fe99d3bb42be99cf196a54291b634618f311a
kishan <kishanbapodra2002@gmail.com>
2022-06-23 01:12:44 +0400

    Fix: dublicate data in watchlist
    Handle cases where a movie/show is already in a watchlist

--- src/components/cards/card.js
+++ src/components/cards/card.js
@@ -5,7 +5,11 @@ import { WatchlistContext } from "../context/WatchlistState";
 
 const Card = ({data ,title, description, image}) => {
 
-  const { addToWatchlist } = useContext(WatchlistContext);
+  const { addToWatchlist, watchlist } = useContext(WatchlistContext);
+
+  let storedMovie = watchlist.find(i => i.title === title)
+  let storedShow = watchlist.find(i => i.original_name === title)
+  const btnDisable = storedMovie || storedShow ? true : false;
 
   return(
         <div className="relative group flex flex-column w-[10rem] h-[24rem] shadow-md hover:shadow-[0_45px_85px_-18px_rgba(0,0,0,1)] text-ellipsis overflow-hidden">
@@ -14,13 +18,10 @@ const Card = ({data ,title, description, image}) => {
                 <img className="rounded-t-lg align-middle" src={"https://image.tmdb.org/t/p/w200/"+image} alt=""/>
                 <div className="p-6">
                   <h5 className="flex justify-center items-center w-full h-full text-gray-900 text-lg font-medium mb-2">{title}</h5>
-                  {/* <p className="text-gray-700 text-base mb-4">
-                    {description}
-                  </p> */}
                 </div>
               </Link>
             </div>
-            <button onClick={() => addToWatchlist(data)} className="hidden rounded-lg group-hover:block absolute hover:cursor-pointer top- w-full h-10 left-1/2 bg-gray-200 bg-opacity-70 -translate-x-1/2 -translate-y-1/2 ">Add to watchlist</button>  
+            <button disabled={btnDisable} onClick={() => addToWatchlist(data)} className="hidden rounded-lg group-hover:block absolute hover:cursor-pointer top- w-full h-10 left-1/2 bg-gray-200 bg-opacity-70 -translate-x-1/2 -translate-y-1/2 ">Add to watchlist</button>  
         </div>
   );
 }
