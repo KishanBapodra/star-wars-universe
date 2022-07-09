4e4b606d2cf8c53d2012d37d9a8cabaa3d741f47
kishan <kishanbapodra2002@gmail.com>
2022-07-09 15:32:55 +0400

    Style: replicate movie page style

--- src/components/tvShows/show.js
+++ src/components/tvShows/show.js
@@ -7,13 +7,20 @@ const Show = (props) => {
     const data = location.state?.data;
 
     return(
-        <div className=" p-36">
-            <h1>
-                {data ? data.original_name : "Loading..."}
-            </h1>
-            <p>
-                {data ? data.overview : "Loading..."}
-            </p>
+        <div className="py-24 bg-red-500/80 h-screen w-full">
+            <div className="h-full flex justify-around items-center mx-20">
+                <div className="h-48 min-h-full sm:min-h-fit bg-slate-700 relative overflow-y-scroll scrollbar-hide max-w-3xl rounded-3xl border-2 border-blue-400 text-white/80 shadow-blue-700 shadow-xl">
+                    <h1 className="flex content-center justify-center font-bold text-2xl p-4 ">
+                        {data ? data.original_name : "Loading..."}
+                    </h1>
+                    <p className=" p-4 font-normal text-justify">
+                        {data ? data.overview : "Loading..."}
+                    </p>
+                </div>
+                <div className="ml-8 md:min-h-fit hidden sm:block sm:min-w-[23%] sm:min-h-[50%] lg:min-h-fit lg:min-w-fit">
+                    <img className="rounded-t-lg align-middle" src={"https://image.tmdb.org/t/p/w300/"+data.poster_path} alt=""/>
+                </div>
+            </div>
         </div>
     )
 }
