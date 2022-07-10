277455662f528d68d9106254dbf737bdbf574562
kishan <kishanbapodra2002@gmail.com>
2022-07-10 10:12:41 +0400

    Style: minor changes
    Symmetric design

--- src/components/watchlist/Watchlist.js
+++ src/components/watchlist/Watchlist.js
@@ -8,11 +8,11 @@ const Watchlist = () => {
     
     return(
         <div className="bg-star-wars-4 bg-cover">
-            <div className="flex flex-wrap justify-around sm:m-0 sm:grid pt-[7.2rem] pb-10 w-full 3xl:grid-cols-8 2xl:grid-cols-7 xl:grid-cols-6 lg:grid-cols-5 md:grid-cols-4 sm:grid-cols-3">   
+            <div className="flex flex-wrap justify-around sm:px-10 sm:m-0 sm:grid pt-[7.2rem] pb-10 w-full 3xl:grid-cols-8 2xl:grid-cols-7 xl:grid-cols-6 lg:grid-cols-5 md:grid-cols-4 sm:grid-cols-3">   
                     {watchlist.map((item, index) => {
                         let isShow = item.original_title === undefined ? true : false;
                         return(
-                            <div key={index} className="sm:pl-10 mt-5">
+                            <div key={index} className="sm:ml-0 sm:px-5 mt-5">
                                 {!isShow ? <Card className="w-96" data={item} title={item.original_title} description={item.overview} image={item.poster_path} /> 
                                 : <Card className="w-96" data={item} title={item.original_name} description={item.overview} image={item.poster_path} /> }
                             </div>
