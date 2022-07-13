edadc927295d49ca3106c6ca76499a58f2760d48
kishan <kishanbapodra2002@gmail.com>
2022-07-13 19:21:56 +0400

    Style: improve responsiveness of show/movie page

--- src/components/movies/movie.js
+++ src/components/movies/movie.js
@@ -5,20 +5,19 @@ const Movie = (props) => {
 
     const location = useLocation();
     const data = location.state?.data;
-    console.log(data)
     return(
-        <div className="py-24 bg-red-500/80 h-screen w-full">
-            <div className="h-full flex justify-around items-center mx-20">
-                <div className="h-48 min-h-full sm:min-h-fit bg-slate-700 relative overflow-y-scroll scrollbar-hide max-w-3xl rounded-3xl border-2 border-blue-400 text-white/80 shadow-blue-700 shadow-xl">
-                    <h1 className="flex content-center justify-center font-bold text-2xl p-4 ">
+        <div className="py-24 bg-sky-900 h-screen w-full">
+            <div className="h-full flex flex-col my-4 sm:flex-row sm:justify-around items-center mx-20">
+                <div className="my-4 sm:my-0 h-48 min-h-[50%] sm:min-h-fit bg-cyan-300 relative overflow-y-scroll scrollbar-hide max-w-3xl rounded-3xl border-2 border-red-400 text-white/80 shadow-red-800 shadow-xl">
+                    <h1 className="flex content-center justify-center font-extrabold text-sky-900 text-2xl p-4 ">
                         {data ? data.original_title : "Loading..."}
                     </h1>
-                    <p className=" p-4 font-normal text-justify">
+                    <p className="pt-2 px-4 font-semibold text-sky-900 text-justify">
                         {data ? data.overview : "Loading..."}
                     </p>
                 </div>
-                <div className="ml-8 md:min-h-fit hidden sm:block sm:min-w-[23%] sm:min-h-[50%] lg:min-h-fit lg:min-w-fit">
-                    <img className="rounded-t-lg align-middle" src={"https://image.tmdb.org/t/p/w300/"+data.poster_path} alt=""/>
+                <div className="my-2 sm:my-0 sm:ml-8 md:min-h-fit sm:block sm:min-w-[23%] sm:min-h-[50%] lg:min-h-fit lg:min-w-fit">
+                    <img className="shadow-lg shadow-red-800 rounded-t-lg max-h-[75%] mx-8 align-middle" src={"https://image.tmdb.org/t/p/w300/"+data.poster_path} alt=""/>
                 </div>
             </div>
         </div>
