93e05a37f7348d3faaab1a39a57271af709e555b
kishan <kishanbapodra2002@gmail.com>
2022-07-14 22:50:33 +0400

    Style: responsive styling

--- src/components/movies/movie.js
+++ src/components/movies/movie.js
@@ -7,7 +7,7 @@ const Movie = (props) => {
     const data = location.state?.data;
     return(
         <div className="py-24 bg-sky-900 h-screen w-full">
-            <div className="h-full flex flex-col my-4 sm:flex-row sm:justify-around items-center mx-20">
+            <div className="h-full flex flex-col my-4 sm:flex-row sm:justify-around items-center mx-10 lg:mx-20">
                 <div className="my-4 sm:my-0 h-48 min-h-[50%] sm:min-h-fit bg-cyan-300 relative overflow-y-scroll scrollbar-hide max-w-3xl rounded-3xl border-2 border-red-400 text-white/80 shadow-red-800 shadow-xl">
                     <h1 className="flex content-center justify-center font-extrabold text-sky-900 text-2xl p-4 ">
                         {data ? data.original_title : "Loading..."}
@@ -16,8 +16,8 @@ const Movie = (props) => {
                         {data ? data.overview : "Loading..."}
                     </p>
                 </div>
-                <div className="my-2 sm:my-0 sm:ml-8 md:min-h-fit sm:block sm:min-w-[23%] sm:min-h-[50%] lg:min-h-fit lg:min-w-fit">
-                    <img className="shadow-lg shadow-red-800 rounded-t-lg max-h-[75%] mx-8 align-middle" src={"https://image.tmdb.org/t/p/w300/"+data.poster_path} alt=""/>
+                <div className="flex justify-center mt-5 my-2 sm:my-0 sm:ml-8 md:min-h-fit sm:block sm:min-w-[23%] sm:min-h-[50%] lg:min-h-fit lg:min-w-fit">
+                    <img className="shadow-lg shadow-red-800 rounded-t-lg max-h-[75%] sm:mx-3 lg:mx-8 align-middle" src={"https://image.tmdb.org/t/p/w300/"+data.poster_path} alt=""/>
                 </div>
             </div>
         </div>
--- src/components/tvShows/show.js
+++ src/components/tvShows/show.js
@@ -8,7 +8,7 @@ const Show = (props) => {
 
     return(
         <div className="py-24 bg-[#580000] h-screen w-full">
-            <div className="h-full flex flex-col my-4 sm:my-0 sm:flex-row sm:justify-around items-center mx-20">
+            <div className="h-full flex flex-col my-4 sm:my-0 sm:flex-row sm:justify-around items-center mx-10 lg:mx-20">
                 <div className="my-4 sm:my-0 h-48 min-h-[50%] sm:min-h-fit bg-red-500/80 relative overflow-y-scroll scrollbar-hide max-w-3xl rounded-3xl border-[3px] border-blue-800 text-white/80 shadow-blue-900 shadow-3xl">
                     <h1 className="flex content-center justify-center font-extrabold text-[#580000] text-2xl p-4 ">
                         {data ? data.original_name : "Loading..."}
@@ -17,8 +17,8 @@ const Show = (props) => {
                         {data ? data.overview : "Loading..."}
                     </p>
                 </div>
-                <div className="my-2 sm:my-0 sm:ml-8 md:min-h-fit sm:block sm:min-w-[23%] sm:min-h-[50%] lg:min-h-fit lg:min-w-fit">
-                    <img className="shadow-xl shadow-blue-900 rounded-t-lg max-h-[75%] mx-8 align-middle" src={"https://image.tmdb.org/t/p/w300/"+data.poster_path} alt=""/>
+                <div className="flex justify-center mt-5 sm:my-0 sm:ml-8 md:min-h-fit sm:block sm:min-w-[23%] sm:min-h-[50%] lg:min-h-fit lg:min-w-fit">
+                    <img className="shadow-xl shadow-blue-900 rounded-t-lg max-h-[75%] sm:mx-3 lg:mx-8 align-middle" src={"https://image.tmdb.org/t/p/w300/"+data.poster_path} alt=""/>
                 </div>
             </div>
         </div>
